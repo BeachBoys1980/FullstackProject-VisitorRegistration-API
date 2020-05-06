6cbd3ebaa9294b3c4b08125b714cec8c474c3b07
BeachBoys1980 <shades_@hotmail.com>
2020-05-06 18:08:50 +0800

    3RD COMMIT: LOGIN, LOGOUT, ADMIN w GET/CREATE, USER w GET/TRACE/REGISTER, CONTROLLERS, PROTECT-ROUTE

---
+++ controllers/users.controllers.js
@@ -0,0 +1,81 @@
+const UserModel = require("../models/user.model");
+
+const { getJWTSecret } = require("../config/jwt");
+const jwt = require("jsonwebtoken");
+
+// GET ALL USERS
+const getAllUsers = async (req, res) => {
+  const results = await UserModel.find();
+  res.status(200).json(results);
+};
+
+// CREATE A NEW USER
+const createUser = async (req, res, next) => {
+  try {
+    const user = new UserModel(req.body);
+    await UserModel.init();
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// LOGIN AND LOGOUT
+const createJWTToken = (username) => {
+  const today = new Date();
+  const exp = new Date(today);
+
+  const secret = getJWTSecret();
+  exp.setDate(today.getDate() + 60);
+
+  const payload = { username: username, exp: parseInt(exp.getTime() / 1000) };
+  const token = jwt.sign(payload, secret);
+  return token;
+};
+
+const bcrypt = require("bcryptjs");
+
+const loginUser = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
+
+    const result = await bcrypt.compare(password, user.password);
+
+    if (!result) {
+      throw new Error("Login failed");
+    }
+
+    const token = createJWTToken(user.username);
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneWeek = oneDay * 7;
+    const expiryDate = new Date(Date.now() + oneWeek);
+
+    // Can expiry date on cookie be changed? How about JWT token?
+    res.cookie("token", token, {
+      expires: expiryDate,
+      httpOnly: true,
+      //signed: true,
+    });
+
+    res.json({ userType: user.userType });
+  } catch (err) {
+    if (err.message === "Login failed") {
+      err.statusCode = 400;
+    }
+    next(err);
+  }
+};
+
+const logoutUser = (req, res) => {
+  res.clearCookie("token").send("You are now logged out!");
+};
+
+module.exports = {
+  getAllUsers,
+  createUser,
+  loginUser,
+  logoutUser,
+};
---
+++ controllers/visits.controller.js
@@ -0,0 +1,80 @@
+const VisitModel = require("../models/visit.model");
+
+// GET ALL VISITS
+const getAllVisits = async (req, res) => {
+  const results = await VisitModel.find();
+  res.status(200).json(results);
+};
+
+// REGISTER A NEW VISIT
+const registerNewVisit = async (req, res, next) => {
+  try {
+    const visit = new VisitModel(req.body);
+    await VisitModel.init();
+    const newVisit = await visit.save();
+    res.status(201).json(newVisit);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// CONTACT TRACE
+const traceNricVisitDate = async (req, res, next) => {
+  const nricToFind = req.params.nric;
+  const dateToFind = new Date(req.query.contactTraceDate);
+
+  const dayAfter = new Date(req.query.contactTraceDate);
+  dayAfter.setDate(dayAfter.getDate() + 1);
+
+  console.log(dateToFind.toUTCString(), dayAfter.toUTCString());
+
+  const infectedVisitor = await VisitModel.findOne({
+    nric: nricToFind,
+    visitDateTime: { $gte: dateToFind, $lt: dayAfter },
+  });
+
+  try {
+    // check if visitor can be found
+    if (!infectedVisitor) {
+      const noUserError = new Error("No such visitor");
+      noUserError.statusCode = 404;
+      throw noUserError;
+    }
+
+    //set parameter of minutes limit to trace
+    const minutesLimitToTrace = 5;
+
+    //set infected visit date/time
+    const infectedDateTime = new Date(infectedVisitor.visitDateTime);
+
+    //get all visits
+    const allVisits = await VisitModel.find();
+
+    //compare infected's visit date/time with each visit's date/time
+    const contactTraceVisits = allVisits.filter((result) => {
+      const visitDateTime = new Date(result.visitDateTime);
+
+      const milliSecondsDifference = Math.abs(infectedDateTime - visitDateTime);
+
+      //   60 seconds/minute * 1000 msecs/second
+      const minutesDifference = Math.floor(
+        milliSecondsDifference / (1000 * 60)
+      );
+
+      //if difference is less or equal to paramter limit, from infected date/time
+      if (minutesDifference <= minutesLimitToTrace) {
+        return result.toObject();
+      }
+    });
+
+    res.json(contactTraceVisits);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllVisits,
+  registerNewVisit,
+  traceNricVisitDate,
+};
